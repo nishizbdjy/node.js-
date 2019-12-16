83495bf5f90e6edf42da44f8a38aa7f29134dd7d
lbw <2659154921@qq.com>
2019-12-16 23:00:50 +0800

    完成请求优化

--- app.js
+++ app.js
@@ -14,41 +14,50 @@ server.on('request', (req, res) => {//监听用户请求
             if (err) return console.log(err.massage)
             res.end(data)
         })
-    } else if ((url === '/add' || url === '/add.html') && method === 'GET') {//add
-        fs.readFile(path.join(__dirname, './views/add.html'), 'utf-8', (err, data) => {
-            if (err) return console.log(err.massage)
-            res.end(data)
-        })
-    } else if ((url === '/edit' || url === '/edit.html') && method === 'GET') {//edit
-        fs.readFile(path.join(__dirname, './views/edit.html'), 'utf-8', (err, data) => {
-            if (err) return console.log(err.massage)
-            res.end(data)
-        })
-    } else if ((url === '/info' || url === '/info.html') && method === 'GET') {//info
-        fs.readFile(path.join(__dirname, './views/info.html'), 'utf-8', (err, data) => {
-            if (err) return console.log(err.massage)
-            res.end(data)
-        })
-    } else if (url === '/node_modules/bootstrap/dist/css/bootstrap.css' && method === 'GET') {//bootstrap
-        fs.readFile(path.join(__dirname, './node_modules/bootstrap/dist/css/bootstrap.css'), 'utf-8', (err, data) => {
-            if (err) return console.log(err.massage)
-            res.end(data)
-        })
-    } else if (url === '/node_modules/jquery/dist/jquery.js' && method === 'GET') {
-        fs.readFile(path.join(__dirname, './node_modules/jquery/dist/jquery.js'), 'utf-8', (err, data) => {//jQuery
-            if (err) return console.log(err.massage)
-            res.end(data)
-        })
-    } else if (url === '/favicon.ico' && method === 'GET') {
-        res.end('')
     }
-    // else {
-    //     console.log(path.join(__dirname, url))
-    //     fs.readFile(path.join(__dirname, url), 'utf-8', (err, data) => {
+    // else if ((url === '/add' || url === '/add.html') && method === 'GET') {//add
+    //     fs.readFile(path.join(__dirname, './views/add.html'), 'utf-8', (err, data) => {
     //         if (err) return console.log(err.massage)
     //         res.end(data)
     //     })
-    // }
+    // } else if ((url === '/edit' || url === '/edit.html') && method === 'GET') {//edit
+    //     fs.readFile(path.join(__dirname, './views/edit.html'), 'utf-8', (err, data) => {
+    //         if (err) return console.log(err.massage)
+    //         res.end(data)
+    //     })
+    // } else if ((url === '/info' || url === '/info.html') && method === 'GET') {//info
+    //     fs.readFile(path.join(__dirname, './views/info.html'), 'utf-8', (err, data) => {
+    //         if (err) return console.log(err.massage)
+    //         res.end(data)
+    //     })
+    // } else if (url === '/node_modules/bootstrap/dist/css/bootstrap.css' && method === 'GET') {//bootstrap
+    //     fs.readFile(path.join(__dirname, './node_modules/bootstrap/dist/css/bootstrap.css'), 'utf-8', (err, data) => {
+    //         if (err) return console.log(err.massage)
+    //         res.end(data)
+    //     })
+    // } else if (url === '/node_modules/jquery/dist/jquery.js' && method === 'GET') {
+    //     fs.readFile(path.join(__dirname, './node_modules/jquery/dist/jquery.js'), 'utf-8', (err, data) => {//jQuery
+    //         if (err) return console.log(err.massage)
+    //         res.end(data)
+    //     })
+    /*}*/ else if (url === '/favicon.ico' && method === 'GET') {
+        res.end('')
+    }
+    else {
+        if (!( url.startsWith('/add') || url.startsWith('/edit') || url.startsWith('/index') || url.startsWith('/info'))) {
+            fs.readFile(path.join(__dirname, url), 'utf-8', (err, data) => {
+                if (err) return console.log(err.massage)
+                res.end(data)
+            })
+        } else {
+            // console.log(path.join(__dirname, url))
+            fs.readFile(path.join(__dirname, './views' + url), 'utf-8', (err, data) => {
+                if (err) return console.log(err.massage)
+                res.end(data)
+            })
+        }
+
+    }
 })
 
 
