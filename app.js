0820477e821fd048efbe4e73957ebea61a2e611d
lbw <2659154921@qq.com>
2019-12-16 21:30:59 +0800

    完成web服务器的搭建及请求的读取

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
-const http = require('http')//引入
+const http = require('http')//http
 const fs = require('fs')//读写文件
+const path = require('path')
+const template = require('art-template')//模板引擎
+const server = http.createServer()//创建一个web服务器
+server.listen(3010, () => {//监听端口
+    console.log('http://127.0.0.1:3010')
+})
+server.on('request', (req, res) => {//监听用户请求
+    let url = req.url//路径
+    let method = req.method//方式
+    if ((url === '/' || url === '/index.html' || url === '/index') && method === 'GET') {//首页的判断
+        fs.readFile(path.join(__dirname, './views/index.html'), 'utf-8', (err, data) => {
+            if (err) return console.log(err.massage)
+            res.end(data)
+        })
+    } else {
+        console.log(path.join(__dirname, url))
+        fs.readFile(path.join(__dirname, url), 'utf-8', (err, data) => {
+            if (err) return console.log(err.massage)
+            res.end(data)
+        })
+    }
+})
