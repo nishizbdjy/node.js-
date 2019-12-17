a604a4a822585b30fb5e5251fa7bf5568f086616
lbw <2659154921@qq.com>
2019-12-17 21:38:47 +0800

    完成数据渲染页面的封装

--- app.js
+++ app.js
@@ -3,43 +3,47 @@ const fs = require('fs')//读写文件
 const path = require('path')//拼接路径模块
 const template = require('art-template')//模板引擎
 const urlm = require('url')//url模块
+const bindRender = require('./bindRender')//读取数据渲染页面
+
 const server = http.createServer()//创建一个web服务器
+
 server.listen(3010, () => {//监听端口
     console.log('http://127.0.0.1:3010')
 })
+
 server.on('request', (req, res) => {//监听用户请求
     let url = req.url//路径
     let method = req.method//方式
     let query = urlm.parse(url, true).query//get请求&后面的值
     let pathname = urlm.parse(url, true).pathname//get请求&前面的地址
-    console.log(query)
-    console.log(pathname)
+
     if ((pathname === '/' || pathname === '/index.html' || pathname === '/index') && method === 'GET') {//首页的判断
-        // fs.readFile(path.join(__dirname, './views/index.html'), 'utf-8', (err, data) => {
-        //     if (err) return console.log(err.massage)
-        //     res.end(data)
-        // })
+
         fs.readFile(path.join(__dirname, './heros.json'), 'utf-8', (err, data) => {//读取数据
             if (err) return console.log(err.message);
             let shuju = JSON.parse(data)            //转换为数组   [{}{}{}]   换{obj : [{},{},{}]}
-            let str = template(path.join(__dirname, './views/index.html'), { data: shuju })//渲染模板
-            res.end(str)                           //返回
+            // let str = template(path.join(__dirname, './views/index.html'), { data: shuju })//渲染模板
+            // res.end(str)                           //返回
+            bindRender('index',{data:shuju},res)
         })
     } else if ((pathname === '/add' || pathname === '/add.html') && method === 'GET') {//英雄添加页
-        fs.readFile(path.join(__dirname, './views/add.html'), 'utf-8', (err, data) => {
-            if (err) return console.log(err.message)
-            res.end(data)
-        })
+        // fs.readFile(path.join(__dirname, './views/add.html'), 'utf-8', (err, data) => {
+        //     if (err) return console.log(err.message)
+        //     res.end(data)
+        // })
+        bindRender('add',{},res)
     } else if ((pathname === '/edit' || pathname === '/edit.html') && method === 'GET') {//英雄编辑页
-        fs.readFile(path.join(__dirname, './views/edit.html'), 'utf-8', (err, data) => {
-            if (err) return console.log(err.message)
-            res.end(data)
-        })
+        // fs.readFile(path.join(__dirname, './views/edit.html'), 'utf-8', (err, data) => {
+        //     if (err) return console.log(err.message)
+        //     res.end(data)
+        // })
+        bindRender('edit',{},res)
     } else if ((pathname === '/info' || pathname === '/info.html') && method === 'GET') {//英雄查看页
-        fs.readFile(path.join(__dirname, './views/info.html'), 'utf-8', (err, data) => {
-            if (err) return console.log(err.message)
-            res.end(data)
-        })
+        // fs.readFile(path.join(__dirname, './views/info.html'), 'utf-8', (err, data) => {
+        //     if (err) return console.log(err.message)
+        //     res.end(data)
+        // })
+        bindRender('info',{},res)
     } else if (pathname.startsWith('/node_modules') && method === 'GET') {// 样式以node开头的
         fs.readFile(path.join(__dirname, pathname), 'utf-8', (err, data) => {
             if (err) return console.log(err.message)
---
+++ bindRender.js
@@ -0,0 +1,14 @@
+//第三方模块
+
+//引入
+const path = require('path')//拼接路径模块
+const template = require('art-template')//模板引擎
+
+//读取数据渲染页面
+function bindRender (file,shuju,res){
+ let str = template(path.join(__dirname, './views/'+file+'.html'),shuju)//渲染模板
+ res.end(str)                 //返回
+}
+
+//暴露方法给外部使用
+module.exports = bindRender;
