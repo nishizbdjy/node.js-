cb2b9db1f785c8f5e792d5a5ea110054bbbf8439
lbw <2659154921@qq.com>
2019-12-17 19:37:48 +0800

    完成首页渲染

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const http = require('http')//http
 const fs = require('fs')//读写文件
-const path = require('path')
+const path = require('path')//拼接路径模块
 const template = require('art-template')//模板引擎
+const urlm = require('url')//url模块
 const server = http.createServer()//创建一个web服务器
 server.listen(3010, () => {//监听端口
     console.log('http://127.0.0.1:3010')
@@ -9,12 +10,25 @@ server.listen(3010, () => {//监听端口
 server.on('request', (req, res) => {//监听用户请求
     let url = req.url//路径
     let method = req.method//方式
-    if ((url === '/' || url === '/index.html' || url === '/index') && method === 'GET') {//首页的判断
-        fs.readFile(path.join(__dirname, './views/index.html'), 'utf-8', (err, data) => {
-            if (err) return console.log(err.massage)
-            res.end(data)
-        })
+    let query = urlm.parse(url,true).query//get请求&后面的值
+    let pathname = urlm.parse(url,true).pathname//get请求&前面的地址
+    console.log(pathname)
+    if ((pathname === '/' || pathname === '/index.html' || pathname === '/index') && method === 'GET') {//首页的判断
+        // fs.readFile(path.join(__dirname, './views/index.html'), 'utf-8', (err, data) => {
+        //     if (err) return console.log(err.massage)
+        //     res.end(data)
+        // })
+       fs.readFile(path.join(__dirname , './heros.json'),'utf-8',(err,data)=>{//读取数据
+           if(err) return console.log(err.message);
+           let shuju = JSON.parse(data)            //转换为数组   [{}{}{}]   换{obj : [{},{},{}]}
+           console.log({data : shuju})
+           let str = template(path.join(__dirname ,'./views/index.html'),{data : shuju})//渲染模板
+           res.end(str)                           //返回
+       })
+    }else {
+        res.end('404')
     }
+})
     // else if ((url === '/add' || url === '/add.html') && method === 'GET') {//add
     //     fs.readFile(path.join(__dirname, './views/add.html'), 'utf-8', (err, data) => {
     //         if (err) return console.log(err.massage)
@@ -40,24 +54,6 @@ server.on('request', (req, res) => {//监听用户请求
     //         if (err) return console.log(err.massage)
     //         res.end(data)
     //     })
-    /*}*/ else if (url === '/favicon.ico' && method === 'GET') {
-        res.end('')
-    }
-    else {
-        if (!( url.startsWith('/add') || url.startsWith('/edit') || url.startsWith('/index') || url.startsWith('/info'))) {
-            fs.readFile(path.join(__dirname, url), 'utf-8', (err, data) => {
-                if (err) return console.log(err.massage)
-                res.end(data)
-            })
-        } else {
-            // console.log(path.join(__dirname, url))
-            fs.readFile(path.join(__dirname, './views' + url), 'utf-8', (err, data) => {
-                if (err) return console.log(err.massage)
-                res.end(data)
-            })
-        }
-
-    }
-})
+/*}*/
 
 
